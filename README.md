ea73705e7101b51972e2152bd5bdbf26ec899e1e
x d n c o <88914723+xdnco@users.noreply.github.com>
2022-09-15 19:38:37 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Linked Up 🔗
+
+Linked Up is a minimalist link in bio theme/template built with [Astro](https://astro.build) that can be used for social media sites like Instagram to quickly and easily share you latest links, news, promos, etc. along with your other social media profiles.
+
+View the [demo site](https://linkedup.littlesticks.dev)
+
+Watch [this YouTube video](https://www.youtube.com/watch?v=elKCpg7TerA) to see how it was made!
---
+++ src/links.js
@@ -0,0 +1,48 @@
+export const mainContent = {
+  imgSrc: `/assets/profile.png`,
+  name: `Little Sticks`,
+  tagline: `Web Development Learning Platform`
+};
+
+export const socialLinks = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com/jaydanurwin/',
+  },
+  {
+    title: 'Twitter',
+    icon: 'mdi:twitter  ',
+    url: 'https://twitter.com/littlesticksdev',
+  },
+  {
+    title: 'YouTube',
+    icon: 'mdi:youtube',
+    url: 'https://youtube.com/c/LittleSticks',
+  },
+  {
+    title: 'GitHub',
+    icon: 'mdi:github',
+    url: 'https://github.com/littlesticks',
+  }
+]
+
+export const links = [
+  {
+    text: 'Home',
+    url: 'https://littlesticks.dev',
+    featured: true,
+  },
+  {
+    text: 'About',
+    url: 'https://jaydanurwin.com/about',
+  },
+  {
+    text: 'Uses',
+    url: 'https://jaydanurwin.com/uses',
+  },
+  {
+    text: 'See my latest blog post!',
+    url: 'https://jaydanurwin.com/about',
+  }
+]
